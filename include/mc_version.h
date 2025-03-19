b3cc50226709da20c9d4c0fcc94dbba5d175158e
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-03-19 16:42:59 +0000

    [ADD]: Version file is added

---
+++ include/mc_version.h
@@ -0,0 +1,14 @@
+#ifndef MC_VERSION_H_
+#define MC_VERSION_H_
+
+
+typedef enum
+{
+  MC_VERSION_PATCH = 0,
+  MC_VERSION_MINOR = 0,
+  MC_VERSION_MAJOR = 1,
+}mc_version;
+
+
+#endif /* MC_VERSION_H_ */
+
