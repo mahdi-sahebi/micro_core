dc84be810e217a646508f30d22c90f74986f163e
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-03-21 08:20:46 +0330

    [UPDATE]: Span module is implemented

--- include/dsa/span.h
+++ include/dsa/span.h
@@ -1,6 +1,20 @@
 #ifndef MC_SPAN_H_
 #define MC_SPAN_H_
 
+#include <stdbool.h>
+#include <stdint.h>
+
+
+typedef struct
+{
+    void*    data;
+    uint32_t size;// u32? opt
+}mc_span;
+
+
+#define mc_span(DATA, SIZE)                 (mc_span){.data = (DATA), .size = (SIZE)}
+bool    mc_span_is_empty(mc_span buffer);
+bool    mc_span_is_null(mc_span buffer);
 
 
 #endif /* MC_SPAN_H_ */
