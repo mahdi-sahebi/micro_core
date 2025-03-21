a0b97879a986b7bca3f0e8078f7796bccff16c76
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-03-21 08:20:46 +0330

    [UPDATE]: Span module is implemented

---
+++ src/dsa/span.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "dsa/span.h"
+
+bool mc_span_is_empty(mc_span buffer)
+{
+    return (0 == buffer.size);
+}
+
+bool mc_span_is_null(mc_span buffer)
+{
+    return (NULL == buffer.data);
+}
+
