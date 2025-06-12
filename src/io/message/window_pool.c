96c5b42777d05f3ee375e5f231a1ca7db1894409
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-06-13 01:22:33 +0330

    [UPDATE]: Members of window pool are renamed

--- src/io/message/window_pool.c
+++ src/io/message/window_pool.c
@@ -9,20 +9,21 @@ static inline wnd_t* get_window(const wndpool_t* const this, uint16_t index)
 
 static void advance_end_window(wndpool_t* this)
 {
-  this->next_window_id++;
+  this->end_id++;
   this->end_index = (this->end_index + 1) % this->capacity;
   this->count++;
 }
 
 void wndpool_clear(wndpool_t* const this)
 {
+  // TODO(MN): Ignore this loop
   for (uint32_t index = 0; index < this->capacity; index++) {
     wnd_clear(get_window(this, index));
   }
 
-  this->begin_window_id = 0;
-  this->next_window_id  = 0;
-  this->begin_index     = 0;
+  this->bgn_id = 0;
+  this->end_id  = 0;
+  this->bgn_index     = 0;
   this->end_index       = 0;
   this->count           = 0;
 }
@@ -50,7 +51,7 @@ wnd_t* wndpool_get(wndpool_t* const this, id_t id)
 bool wndpool_enqueue(wndpool_t* const this, const mc_span data)
 {
   wnd_t* const window = get_window(this, this->end_index);
-  wnd_write(window, data, this->next_window_id);
+  wnd_write(window, data, this->end_id);
   return true;
 }
 
@@ -66,38 +67,38 @@ uint32_t wndpool_get_capacity(wndpool_t* const this)
 
 id_t wndpool_get_bgn_id(wndpool_t* const this)
 {
-  return this->begin_window_id;
+  return this->bgn_id;
 }
 
 id_t wndpool_get_end_id(wndpool_t* const this)
 {
-  return this->begin_window_id + this->capacity;
+  return this->bgn_id + this->capacity;
 }
 
 bool wndpool_push(wndpool_t* const this, const mc_span data)
 {
   wnd_t* const window = get_window(this, this->end_index);
-  wnd_write(window, data, this->next_window_id);
+  wnd_write(window, data, this->end_id);
   return true;
 }
 
 bool wndpool_ack(wndpool_t* const this, id_t id, wndpool_on_done_fn on_done)
 {
-  if ((id < this->begin_window_id) || (id >= this->begin_window_id + this->capacity)) {
+  if ((id < this->bgn_id) || (id >= this->bgn_id + this->capacity)) {
     return false;
   }
   
   // TODO(MN): on_done if id==bgn_id
-  if (id == this->begin_window_id) {
+  if (id == this->bgn_id) {
     
   }
 
 
-  const uint32_t window_index = ((id - this->begin_window_id) + this->begin_index) % this->capacity;
+  const uint32_t window_index = ((id - this->bgn_id) + this->bgn_index) % this->capacity;
   wnd_t* const window = get_window(this, window_index);
   if (!wnd_is_acked(window)) {
     wnd_ack(window);
-    // [WINDOW %u-%u] Received ACK for packet %u - %uus\n",              this->begin_window_id, this->begin_window_id+this->capacity-1, window_id, TimeNowU() - window->last_sent_time_us));
+    // [WINDOW %u-%u] Received ACK for packet %u - %uus\n",              this->bgn_id, this->bgn_id+this->capacity-1, window_id, TimeNowU() - window->last_sent_time_us));
   }
 
   return true;
