5155bd28f9bdae8f07901ef73b21e1c31d78d876
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-07-09 03:05:40 +0330

    [UPDATE]: Time implemneted

---
+++ src/core/time/time_linux.c
@@ -0,0 +1,26 @@
+#include "core/time.h"
+
+
+mc_time_t mc_now()
+{
+  struct timeval now;
+  gettimeofday(&now, NULL);
+
+  return ((now.tv_sec * 1000000) + now.tv_usec) * 1000;
+}
+
+mc_time_t mc_now_u()
+{
+  return mc_now() / 1000;
+}
+
+mc_time_t mc_now_m()
+{
+  return mc_now() / 1000000;
+}
+
+mc_time_t mc_now_s()
+{
+  return mc_now() / 1000000000;
+}
+
