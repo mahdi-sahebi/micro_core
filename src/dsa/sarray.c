7821a99a8943ac14822afd899a33e09679685d1e
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-07-17 18:38:32 +0330

    [UPDATE]: APIs are declared

---
+++ src/dsa/sarray.c
@@ -0,0 +1,69 @@
+/* TODO(MN): Reduce meta data size.
+ * Not tested. Implement test cases.
+ */
+
+ #include <stdlib.h>
+#include "dsa/span.h"
+#include "dsa/sarray.h"
+
+
+struct _mc_sarray
+{
+  uint32_t capacity;
+  uint32_t count;
+  uint16_t element_size;
+  char     data[0];
+};
+
+
+mc_result_ptr mc_sarray_new(mc_span buffer, uint32_t element_size, uint32_t capacity, mc_comparator comparator)
+{
+  if (mc_span_is_null(buffer) || mc_span_is_empty(buffer) ||
+      (0 == capacity) || (0 == element_size) || (NULL == comparator)) {
+    return mc_result_ptr(NULL, MC_ERR_INVALID_ARGUMENT);
+  }
+
+  const uint32_t size = sizeof(struct _mc_sarray) + (capacity * element_size);
+
+  if (buffer.size < size) {
+    return mc_result_ptr(NULL, MC_ERR_OUT_OF_RANGE);
+  }
+
+  mc_sarray array     = (mc_sarray)buffer.data;
+  array->capacity     = size;
+  array->count        = 0;
+  array->element_size = element_size;
+
+  return mc_result_ptr(array, MC_SUCCESS);
+}
+
+mc_result mc_sarray_free(mc_sarray* const this)
+{
+  return MC_ERR_INVALID_ARGUMENT;
+}
+
+mc_result mc_sarray_clear(mc_sarray const this)
+{
+  return MC_ERR_INVALID_ARGUMENT;
+}
+
+mc_result_ptr mc_sarray_get(const mc_sarray const this, uint32_t index)
+{
+  return mc_result_ptr(NULL, MC_ERR_INVALID_ARGUMENT);
+}
+
+mc_result mc_sarray_insert(mc_sarray const this, const void* const data, uint32_t index)
+{
+  return MC_ERR_INVALID_ARGUMENT;
+}
+
+mc_result mc_sarray_append(mc_sarray const this, const void* const data)
+{
+  return MC_ERR_INVALID_ARGUMENT;
+}
+
+mc_result mc_sarray_remove(mc_sarray const this, uint32_t index)
+{
+  return MC_ERR_INVALID_ARGUMENT;
+}
+
