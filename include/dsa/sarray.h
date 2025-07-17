9254458fa443806125746351b42605c8298564b6
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-07-17 18:53:29 +0330

    [UPDATE]: Init API is implemented

--- include/dsa/sarray.h
+++ include/dsa/sarray.h
@@ -10,8 +10,7 @@ typedef struct _mc_sarray* mc_sarray;
 typedef bool (*mc_comparator)(const void* const data_1, const void* const data_2);
 
 
-mc_result_ptr mc_sarray_new(mc_span buffer, uint32_t element_size, uint32_t capacity, mc_comparator comparator);
-mc_result     mc_sarray_free(mc_sarray* const this);
+mc_result     mc_sarray_init(mc_sarray* const this, mc_span buffer, uint32_t element_size, uint32_t capacity, mc_comparator comparator);
 mc_result     mc_sarray_clear(mc_sarray const this);
 mc_result_ptr mc_sarray_get(const mc_sarray const this, uint32_t index);
 mc_result     mc_sarray_insert(mc_sarray const this, const void* const data, uint32_t index);
