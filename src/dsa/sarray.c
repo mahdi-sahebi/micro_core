156d75969034b0224f772a46228e998d0b24ea49
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-07-22 02:00:07 +0330

    [UPDATE]: mc_sarray_get_count() is implemented

--- src/dsa/sarray.c
+++ src/dsa/sarray.c
@@ -19,26 +19,25 @@ struct _mc_sarray
 #define GET_DATA(ARRAY, INDEX)   ((ARRAY)->data + ((ARRAY)->data_size * (INDEX)))
 
 
-mc_result mc_sarray_init(mc_sarray* this, mc_span buffer, uint32_t data_size, uint32_t capacity, mc_comparator comparator)
+mc_result_ptr mc_sarray_init(mc_span buffer, uint32_t data_size, uint32_t capacity, mc_comparator comparator)
 {
-  *this = NULL;
   if (mc_span_is_null(buffer) || mc_span_is_empty(buffer) ||
       (0 == capacity) || (0 == data_size) || (NULL == comparator)) {
-    return MC_ERR_INVALID_ARGUMENT;
+    return mc_result_ptr(NULL, MC_ERR_INVALID_ARGUMENT);
   }
 
   const uint32_t size = sizeof(struct _mc_sarray) + (capacity * data_size);
   if (buffer.size < size) {
-    return MC_ERR_OUT_OF_RANGE;
+    return mc_result_ptr(NULL, MC_ERR_OUT_OF_RANGE);
   }
 
-  *this               = (mc_sarray)buffer.data;
-  (*this)->comparator = comparator;
-  (*this)->capacity   = size;
-  (*this)->count      = 0;
-  (*this)->data_size  = data_size;
+  mc_sarray this   = (mc_sarray)buffer.data;
+  this->comparator = comparator;
+  this->capacity   = size;
+  this->count      = 0;
+  this->data_size  = data_size;
 
-  return MC_SUCCESS;
+  return mc_result_ptr(this, MC_SUCCESS);
 }
 
 mc_result mc_sarray_clear(mc_sarray this)
@@ -50,6 +49,15 @@ mc_result mc_sarray_clear(mc_sarray this)
   this->count = 0;
 
   return MC_SUCCESS;
+}
+
+mc_result_u32  mc_sarray_get_count(const mc_sarray this)
+{
+  if (NULL == this) {
+    return mc_result_u32(0, MC_ERR_INVALID_ARGUMENT);
+  }
+
+  return mc_result_u32(this->count, MC_SUCCESS);
 }
 
 mc_result_ptr mc_sarray_get(const mc_sarray this, uint32_t index)
