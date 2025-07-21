b9b4e6379647035db5415b4d4871f45223826ec6
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-07-21 21:34:45 +0330

    [UPDATE]: Module is implemneted

--- include/dsa/sarray.h
+++ include/dsa/sarray.h
@@ -10,12 +10,13 @@ typedef struct _mc_sarray* mc_sarray;
 typedef bool (*mc_comparator)(const void* const data_1, const void* const data_2);
 
 
-mc_result     mc_sarray_init(mc_sarray* const this, mc_span buffer, uint32_t element_size, uint32_t capacity, mc_comparator comparator);
-mc_result     mc_sarray_clear(mc_sarray const this);
-mc_result_ptr mc_sarray_get(const mc_sarray const this, uint32_t index);
-mc_result     mc_sarray_insert(mc_sarray const this, const void* const data, uint32_t index);
-mc_result     mc_sarray_append(mc_sarray const this, const void* const data);
-mc_result     mc_sarray_remove(mc_sarray const this, uint32_t index);
+mc_result      mc_sarray_init(mc_sarray* this, mc_span buffer, uint32_t data_size, uint32_t capacity, mc_comparator comparator);
+mc_result      mc_sarray_clear(mc_sarray this);
+mc_result_ptr  mc_sarray_get(const mc_sarray this, uint32_t index);
+mc_result      mc_sarray_insert(mc_sarray this, const void* data);
+mc_result      mc_sarray_remove(mc_sarray this, uint32_t index);
+mc_result_bool mc_sarray_is_emtpy(const mc_sarray this);
+mc_result_bool mc_sarray_is_full(const mc_sarray this);
 
 
 #endif /* MC_DSA_SORTED_ARRAY_H_ */
--- src/dsa/sarray.c
+++ src/dsa/sarray.c
@@ -9,14 +9,17 @@
 
 struct _mc_sarray
 {
+  mc_comparator comparator;
   uint32_t capacity;
   uint32_t count;
   uint16_t data_size;
   char     data[0];
 };
 
+#define GET_DATA(ARRAY, INDEX)   ((ARRAY)->data + ((ARRAY)->data_size * (INDEX)))
 
-mc_result mc_sarray_init(mc_sarray* const this, mc_span buffer, uint32_t data_size, uint32_t capacity, mc_comparator comparator)
+
+mc_result mc_sarray_init(mc_sarray* this, mc_span buffer, uint32_t data_size, uint32_t capacity, mc_comparator comparator)
 {
   *this = NULL;
   if (mc_span_is_null(buffer) || mc_span_is_empty(buffer) ||
@@ -25,20 +28,20 @@ mc_result mc_sarray_init(mc_sarray* const this, mc_span buffer, uint32_t data_si
   }
 
   const uint32_t size = sizeof(struct _mc_sarray) + (capacity * data_size);
-
   if (buffer.size < size) {
     return MC_ERR_OUT_OF_RANGE;
   }
 
-  *this                 = (mc_sarray)buffer.data;
-  (*this)->capacity     = size;
-  (*this)->count        = 0;
-  (*this)->data_size = data_size;
+  *this               = (mc_sarray)buffer.data;
+  (*this)->comparator = comparator;
+  (*this)->capacity   = size;
+  (*this)->count      = 0;
+  (*this)->data_size  = data_size;
 
   return MC_SUCCESS;
 }
 
-mc_result mc_sarray_clear(mc_sarray const this)
+mc_result mc_sarray_clear(mc_sarray this)
 {
   if (NULL == this) {
     return MC_ERR_INVALID_ARGUMENT;
@@ -49,7 +52,7 @@ mc_result mc_sarray_clear(mc_sarray const this)
   return MC_SUCCESS;
 }
 
-mc_result_ptr mc_sarray_get(const mc_sarray const this, uint32_t index)
+mc_result_ptr mc_sarray_get(const mc_sarray this, uint32_t index)
 {
   if (NULL == this) {
     return mc_result_ptr(NULL, MC_ERR_INVALID_ARGUMENT);
@@ -58,22 +61,84 @@ mc_result_ptr mc_sarray_get(const mc_sarray const this, uint32_t index)
     return mc_result_ptr(NULL, MC_ERR_OUT_OF_RANGE);
   }
 
-  const void* const data = this->data + (this->data_size * index);
-  return mc_result_ptr(data, MC_ERR_INVALID_ARGUMENT);
+  return mc_result_ptr(GET_DATA(this, index), MC_ERR_INVALID_ARGUMENT);
 }
 
-mc_result mc_sarray_insert(mc_sarray const this, const void* const data, uint32_t index)
+mc_result mc_sarray_insert(mc_sarray this, const void* data)
 {
-  return MC_ERR_INVALID_ARGUMENT;
+  if (NULL == this) {
+    return MC_ERR_INVALID_ARGUMENT;
+  }
+  if (this->count >= this->capacity) {
+    return MC_ERR_OUT_OF_RANGE;
+  }
+
+  if (0 == this->count) {
+    memcpy(GET_DATA(this, 0), data, this->data_size);
+  } else {
+    uint32_t bgn = 0;
+    uint32_t end = this->count - 1;
+
+    void* bgn_data = GET_DATA(this, bgn);
+    void* end_data = GET_DATA(this, end);
+
+    for (; bgn != end;) {
+      const uint32_t mid = (bgn + end) >> 1;
+
+      if (this->comparator(data, GET_DATA(this, mid))) {
+        end = mid - 1;
+      } else {
+        bgn = mid + 1;
+      }
+    }
+
+    /* Shift one element to right */
+    for (uint32_t index = this->count - 1; index >= bgn; index--) {
+      memcpy(GET_DATA(this, index + 1), GET_DATA(this, index), this->data_size);
+    }
+
+    memcpy(GET_DATA(this, bgn), data, this->data_size);
+  }
+
+  this->count++;
+  return MC_SUCCESS;
 }
 
-mc_result mc_sarray_append(mc_sarray const this, const void* const data)
+mc_result mc_sarray_remove(mc_sarray this, uint32_t index)
 {
-  return MC_ERR_INVALID_ARGUMENT;
+  if (NULL == this) {
+    return MC_ERR_INVALID_ARGUMENT;
+  }
+  if (0 == this->count) {
+    return MC_ERR_OUT_OF_RANGE;
+  }
+
+  /* Shift one element to left */
+  for (uint32_t itr = index + 1; itr < this->count - 1; itr++) {
+    memcpy(GET_DATA(this, index - 1), GET_DATA(this, index), this->data_size);
+  }
+
+  this->count--;
+  return MC_SUCCESS;
 }
 
-mc_result mc_sarray_remove(mc_sarray const this, uint32_t index)
+mc_result_bool mc_sarray_is_emtpy(const mc_sarray this)
 {
-  return MC_ERR_INVALID_ARGUMENT;
+  if (NULL == this) {
+    return mc_result_bool(false, MC_ERR_INVALID_ARGUMENT);
+  }
+
+  return mc_result_bool(0 == this->count, MC_SUCCESS);
 }
 
+mc_result_bool mc_sarray_is_full(const mc_sarray this)
+{
+  if (NULL == this) {
+    return mc_result_bool(false, MC_ERR_INVALID_ARGUMENT);
+  }
+
+  return mc_result_bool(this->capacity == this->count, MC_SUCCESS);
+}
+
+
+#undef GET_DATA
