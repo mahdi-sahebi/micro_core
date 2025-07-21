0652e54f49e3a889a620d5ba146451836ad9eab8
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-07-22 02:00:07 +0330

    [UPDATE]: mc_sarray_get_count() is implemented

--- include/dsa/sarray.h
+++ include/dsa/sarray.h
@@ -10,8 +10,9 @@ typedef struct _mc_sarray* mc_sarray;
 typedef bool (*mc_comparator)(const void* const data_1, const void* const data_2);
 
 
-mc_result      mc_sarray_init(mc_sarray* this, mc_span buffer, uint32_t data_size, uint32_t capacity, mc_comparator comparator);
+mc_result_ptr  mc_sarray_init(mc_span buffer, uint32_t data_size, uint32_t capacity, mc_comparator comparator);
 mc_result      mc_sarray_clear(mc_sarray this);
+mc_result_u32  mc_sarray_get_count(const mc_sarray this);
 mc_result_ptr  mc_sarray_get(const mc_sarray this, uint32_t index);
 mc_result      mc_sarray_insert(mc_sarray this, const void* data);
 mc_result      mc_sarray_remove(mc_sarray this, uint32_t index);
