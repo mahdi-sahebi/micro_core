b80bd9b31af55d93576e814a79e007ff70fd87a0
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-07-28 19:07:24 +0330

    [UPDATE]: APIs are become macro

--- include/alg/span.h
+++ include/alg/span.h
@@ -1,30 +1,23 @@
-/* TODO(MN): sub-span, rename to m buffer, is in range,...
- * Rename namespace _t
- * Change data to char
- * Move into the core?
- */
-
 #ifndef MC_SPAN_H_
 #define MC_SPAN_H_
 
-// TODO(MN):is in range,...
-// TODO(MN): Without API ?
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 
 typedef struct
 {
-    char*    data;
-    uint32_t count;
-    uint32_t data_size;
+  char*    data;
+  uint32_t count;
+  uint32_t data_size;
 }mc_span;
 
 
 #define mc_span_raw(DATA, COUNT, DATA_SIZE)     (mc_span){.data = (char*)(DATA), .count = (COUNT), .data_size = (DATA_SIZE)}
 #define mc_span(DATA, COUNT)                    mc_span_raw(DATA, COUNT, sizeof(char))
-bool    mc_span_is_empty(mc_span buffer);
-bool    mc_span_is_null(mc_span buffer);
+#define mc_span_is_empty(SPAN)                  (0 == (SPAN).count)
+#define mc_span_is_null(SPAN)                   (NULL == (SPAN).data)
 
 
 #endif /* MC_SPAN_H_ */
