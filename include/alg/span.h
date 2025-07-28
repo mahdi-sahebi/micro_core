a60cc59953be5e52c30bf101c2eb2ffcba821b8c
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-07-28 20:46:39 +0330

    [UPDATE]: COUNT argument is changed to byte size

--- include/alg/span.h
+++ include/alg/span.h
@@ -14,9 +14,8 @@ typedef struct
 }mc_span;
 
 
-// TODO(MN): COUNT -> SIZE in byte
-#define mc_span_raw(DATA, COUNT, DATA_SIZE)     (mc_span){.data = (char*)(DATA), .capacity = (COUNT), .data_size = (DATA_SIZE)}
-#define mc_span(DATA, COUNT)                    mc_span_raw(DATA, COUNT, sizeof(char))
+#define mc_span_raw(ADDRESS, SIZE, DATA_SIZE)   (mc_span){.data = (char*)(ADDRESS), .capacity = (SIZE) / (DATA_SIZE), .data_size = (DATA_SIZE)}
+#define mc_span(ADDRESS, SIZE)                  mc_span_raw(ADDRESS, SIZE, sizeof(char))
 #define mc_span_is_empty(SPAN)                  (0 == (SPAN).capacity)
 #define mc_span_is_null(SPAN)                   (NULL == (SPAN).data)
 #define mc_span_get_size(SPAN)                  ((SPAN).capacity * (SPAN).data_size)
