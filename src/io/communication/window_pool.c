bf40f3e97f12dcb16705d82b412ceff1b8a54138
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-08-04 19:27:21 +0330

    [OPT]: Extra checkings are removed

--- src/io/communication/window_pool.c
+++ src/io/communication/window_pool.c
@@ -23,13 +23,13 @@ static wnd_t* get_window(const wndpool_t* const this, const mc_wnd_idx index)
 
 static bool is_first_acked(const wndpool_t* const this)
 {
-  return wnd_is_acked(get_window(this, this->bgn_index)) &&
-         (INVALID_ID != get_window(this, this->bgn_index)->packet.id);
+  const wnd_t* window = get_window(this, this->bgn_index);
+  return wnd_is_valid(window) && wnd_is_acked(window);
 }
 
 void wndpool_clear(wndpool_t* const this)
 {
-  // TODO(MN): Ignore this loop
+  // TODO(MN): Ignore this loop. separate data and meta list to exploit memcpy 
   for (mc_wnd_idx index = 0; index < this->capacity; index++) {
     wnd_clear(get_window(this, index));
   }
@@ -126,9 +126,7 @@ uint8_t wndpool_get_count(const wndpool_t* const this)
   uint8_t count = 0;
 
   for (mc_wnd_idx index = 0; index < this->capacity; index++) {
-    if (wnd_is_valid(get_window(this, index))) {
-      count++;
-    }
+    count += wnd_is_valid(get_window(this, index));
   }
 
   return count;
@@ -153,16 +151,11 @@ bool wndpool_push(wndpool_t* const this, const mc_span data)
 }
 
 bool wndpool_ack(wndpool_t* const this, mc_pkt_id id, mc_io_receive_cb on_done)
-{
-  if (!wndpool_contains(this, id)) {
-    return false;
-  }
-  
+{  
   const uint32_t window_index = get_index(this, id);
   wnd_t* const window = get_window(this, window_index);
   if (!wnd_is_acked(window)) {
     wnd_ack(window);
-    
     // [WINDOW %u-%u] Received ACK for packet %u - %uus\n",              this->bgn_id, this->bgn_id+this->capacity-1, window_id, TimeNowU() - window->last_sent_time_us));
   }
 
