ede5c12f9ca2366217d5ab2ed3fa469e796cbab8
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-08-14 17:08:18 +0330

    [UPDATE]: Unique input argument is added for each chain node

--- include/pattern/mc_chain.h
+++ include/pattern/mc_chain.h
@@ -5,35 +5,41 @@
 #include <alg/span.h>
 
 
+
 typedef struct
 {
-  void*    arg;
   mc_span  buffer;
   mc_error error;
 }mc_chain_data;
 
-typedef mc_chain_data (*mc_chain_cb)(mc_chain_data);
+typedef mc_chain_data (*mc_chain_cb)(mc_span buffer, void* arg);
 
-typedef struct __attribute__((packed))
+typedef struct
 {
-  uint8_t     capacity;
-  uint8_t     count;
-  mc_chain_cb nodes[0];
+  mc_chain_cb api;
+  void*       arg;
+}mc_chain_node;
+
+typedef struct  // TODO(MN): Remove pads. Align array
+{
+  uint8_t       capacity;
+  uint8_t       count;
+  mc_chain_node nodes[0];
 }mc_chain;
 
 
 // TODO(MN): Don't get error
-#define mc_chain_data(THIS, BUFFER, ERROR)\ 
-  (mc_chain_data){.arg = (THIS), .buffer = (BUFFER), .error = (ERROR)}
+#define mc_chain_data(BUFFER, ERROR)\ 
+  (mc_chain_data){.buffer = (BUFFER), .error = (ERROR)}
 
 #define mc_chain_data_error(ERROR)\
-  mc_chain_data(NULL, mc_span_null(), ERROR)
+  mc_chain_data(mc_span_null(), ERROR)
 
 mc_result_u32 mc_chain_get_alloc_size(uint8_t capacity);
 mc_result_ptr mc_chain_init(mc_span alloc_buffer, uint8_t capacity);
 mc_error      mc_chain_clear(mc_chain* this);
-mc_error      mc_chain_push(mc_chain* this, mc_chain_cb node);
-mc_chain_data mc_chain_run(mc_chain* this, mc_chain_data data);
+mc_error      mc_chain_push(mc_chain* this, mc_chain_cb api, void* arg);
+mc_chain_data mc_chain_run(mc_chain* this, mc_span buffer);
 
 
 #endif /* MC_PATTERN_CHAIN_H_ */
