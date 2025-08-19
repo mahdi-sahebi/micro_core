059de8b9a876df34d840dc1700f31416b1a0f62c
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-08-19 16:36:04 +0330

    [UPDATE]: IO definitions are renamed to have io namespace

--- include/io/io.h
+++ include/io/io.h
@@ -4,19 +4,18 @@
 #include <stdint.h>
 
 
-typedef uint32_t (*mc_mc_transceiver_recv_fn)(void* const data, uint32_t size);
-typedef uint32_t (*mc_mc_transceiver_send_fn)(const void* const data, uint32_t size);
-
+typedef uint32_t (*mc_io_recv_fn)(void* const data, uint32_t size);
+typedef uint32_t (*mc_io_send_fn)(const void* const data, uint32_t size);
 typedef void (*mc_io_receive_cb)(const void* const data, uint32_t size);
 
 typedef struct
 {
-  mc_mc_transceiver_recv_fn recv;
-  mc_mc_transceiver_send_fn send;
+  mc_io_recv_fn recv;
+  mc_io_send_fn send;
 }mc_io;
 
 
-#define mc_io(RECV, SEND)     ((mc_io){.recv = RECV, .send = SEND})
+#define mc_io(RECV, SEND)     ((mc_io){.recv = (RECV), .send = (SEND)})
 
 
 #endif /* MC_IO_H_ */
