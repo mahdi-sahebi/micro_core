9fc5785061bd31be241590a860063ed6c2308245
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-08-21 17:15:03 +0330

    [TEST]: Message tests are implemented

---
+++ test/io/message/udp/test_receiver.c
@@ -0,0 +1,250 @@
+
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "core/error.h"
+#include "core/version.h"
+#include "core/time.h"
+#include "io/message/mc_message.h"
+#include "test_common.h"
+#include "test_receiver.h"
+
+
+static int ServerSocket = -1;
+static uint32_t* Result = NULL;
+static mc_msg* message = NULL;
+static mc_buffer AllocBuffer = {0};
+static uint32_t TestCounter = 0;
+static mc_error Error = MC_SUCCESS;
+static bool IsStringReceived = false;
+static bool IsLargeReceived  = false;
+static bool IsTinyReceived   = false;
+
+
+static void server_create()
+{
+  ServerSocket = socket(AF_INET, SOCK_DGRAM, 0);
+  
+  struct sockaddr_in addr_in;
+  memset(&addr_in, 0, sizeof(addr_in));
+  addr_in.sin_family = AF_INET;
+  addr_in.sin_port = htons(SERVER_PORT);
+  addr_in.sin_addr.s_addr = INADDR_ANY;
+  bind(ServerSocket, (struct sockaddr*)&addr_in, sizeof(addr_in));
+
+  struct timeval timeout = {0, 10000};
+  setsockopt(ServerSocket, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+}
+
+static uint32_t server_write(const void* const data, uint32_t size)
+{
+  return socket_write(ServerSocket, data, size, "127.0.0.1", CLIENT_PORT);
+}
+
+static uint32_t server_read(void* const data, uint32_t size)
+{
+  return socket_read(ServerSocket, data, size);
+}
+static void server_close()
+{
+  close(ServerSocket);
+}
+
+static void flush_receive_buffer()
+{
+  char temp[100];
+  for (uint32_t index = 0; index < 10; index++) {
+    server_read(temp, sizeof(temp));
+  }
+}
+
+static void print_progress(float progress)
+{
+  const uint32_t BAR_LENGTH = 20;
+  const uint32_t num_bars = progress * BAR_LENGTH;
+
+  printf("\r\t\t\t\t\t\t\r");
+
+  for (uint32_t index = 0; index < BAR_LENGTH; index++) {
+    if (index < num_bars) {
+      printf("█");
+    } else {
+      printf("▒");
+    }
+  }
+
+  printf(" %.1f%%", progress * 100);
+  if (1.0F == progress) {
+    printf("\n");
+  }
+  fflush(stdout);
+}
+
+static bool init(void* data)
+{
+  /* TODO(MN): invalid header, incomplete packet, miss packet pointer, use zero copy
+   */
+  Result = (uint32_t*)data;
+  *Result = MC_SUCCESS;
+
+  server_create();
+  flush_receive_buffer();
+
+
+  mc_msg_cfg config =
+  {
+    .io = mc_io(server_read, server_write),
+    .window_size = 37,
+    .recv_pool_size = 120
+  };
+  const mc_result_u32 result_u32 = mc_msg_get_alloc_size(config);
+  if (MC_SUCCESS != result_u32.error) {
+    *Result = result_u32.error;
+    return false;
+  }
+  const uint32_t alloc_size = result_u32.value;
+  AllocBuffer = mc_buffer(malloc(alloc_size), alloc_size);// TODO(MN): Don't alloc dynamically
+
+  const mc_result_ptr result = mc_msg_init(AllocBuffer, config);
+  if (MC_SUCCESS != result.error) {
+    *Result = result.error;
+    return false;
+  }
+
+  message = result.data;
+  return true;
+}
+
+static void print_log()
+{
+  const uint32_t recv_cnt = cfg_get_recv_counter();
+  const uint32_t send_cnt = cfg_get_send_counter();
+  const uint32_t recv_failed_cnt = cfg_get_recv_failed_counter();
+  const uint32_t send_failed_cnt = cfg_get_send_failed_counter();
+  printf("[IO] Completed{Recv: %u, Send: %u} - Failed{Recv: %u(%.1f%%), Send: %u(%.1f%%)}\n",
+      recv_cnt, send_cnt, 
+      recv_failed_cnt, 100 * (recv_failed_cnt / (float)recv_cnt),
+      send_failed_cnt, 100 * (send_failed_cnt / (float)send_cnt)
+    );
+}
+
+static void deinit()
+{
+  server_close();
+  print_log();
+  free(AllocBuffer.data);
+}
+
+static void wait_for_sender()
+{
+  const mc_time_t end_time = mc_now_u() + 500000 * ((cfg_get_loss_rate() / 10) + 1);
+
+  while (mc_now_u() < end_time) {
+    mc_msg_update(message);
+  }
+}
+
+static void on_string_received(mc_msg_id, mc_buffer)
+{
+  char data[9] = {0};
+  const uint32_t size = sizeof(data);
+  
+  if (0 != memcmp(&data[0], "!p", 2)) {
+    printf("[ERR Data 1] wrong data received\n");
+    Error = MC_ERR_RUNTIME;
+  }
+
+  if (0 != memcmp(&data[5], ".?I", 3)) {
+    printf("[ERR Data 1] wrong data received\n");
+    Error = MC_ERR_RUNTIME;
+  }
+
+  char num_text[4] = {0};
+  sprintf(num_text, "%03u", TestCounter % 1000);
+  if (0 != memcmp(num_text, &data[2], 3)) {
+    printf("[ERR Data 1] wrong data received\n");
+    Error = MC_ERR_RUNTIME;
+  }
+
+  IsStringReceived = true;
+}
+
+static void on_large_received(mc_msg_id, mc_buffer)
+{
+  uint32_t data[30] = {0};
+  const uint32_t random_count = (TestCounter * 1664525) + 1013904223;
+  const uint32_t count = (random_count % 28) + 2;
+  const uint32_t size = count * sizeof(*data);
+
+  for (uint32_t index = 0; index < count; index++) {
+    const uint32_t expected = ((index & 1) ? -56374141.31 : +8644397.79) * (index + 1) * (TestCounter + 1) + index;
+    if (data[index] != expected) {
+      printf("[ERR Data 2] Received: %u, Expected: %u\n", data[index], expected);
+      Error = MC_ERR_RUNTIME;
+    }
+  }
+
+  IsLargeReceived = true;
+}
+
+static void on_tiny_received(mc_msg_id, mc_buffer)
+{
+  bool data = false;
+
+  if ((TestCounter & 1) != data) {
+    printf("[ERR Data 1] wrong data received\n");
+    Error = MC_ERR_RUNTIME;
+  }
+
+  IsTinyReceived = true;
+}
+
+static bool all_messages_received()
+{
+  return (IsStringReceived && IsLargeReceived && IsTinyReceived);
+}
+
+static void reset_flags()
+{
+  IsStringReceived = false;
+  IsLargeReceived  = false;
+  IsTinyReceived   = false;
+}
+
+void* rcv_start(void* data)
+{
+  if (!init(data)) {
+    return NULL;
+  }
+
+  for (TestCounter = 0; TestCounter <= cfg_get_iterations(); TestCounter++) {
+    while (!all_messages_received()) {
+      if (MC_SUCCESS != Error) {
+        *Result = Error;
+        break;
+      }
+
+      if (MC_SUCCESS != mc_msg_update(message)) {
+        *Result = MC_ERR_TIMEOUT;
+        break;
+      }
+    }
+    
+    reset_flags();
+    print_progress(TestCounter / (float)cfg_get_iterations());
+  }
+
+  if (MC_SUCCESS == *Result) {
+    const mc_result_bool result = mc_msg_flush(message, TEST_TIMEOUT_US);
+    if ((MC_SUCCESS != result.error) || !result.value) {
+      printf("mc_comm_flush failed\n");
+      *Result = MC_ERR_TIMEOUT;
+    }
+  }
+
+  wait_for_sender();
+  deinit();
+  return NULL;
+}
---
+++ test/io/message/udp/test_sender.h
@@ -0,0 +1,6 @@
+#ifndef MC_TEST_IO_MESSATE_UDP_SENDER_H_
+#define MC_TEST_IO_MESSATE_UDP_SENDER_H_
+
+void* snd_start(void* data);
+
+#endif /* MC_TEST_IO_MESSATE_UDP_SENDER_H_ */
