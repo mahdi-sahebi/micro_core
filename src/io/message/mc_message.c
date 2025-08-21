3b7e22dc0e109a3808457bb4b605cd25a862f1b8
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-08-21 18:31:11 +0330

    [UPDATE]: mc_msg_get_alloc_size() is implemented

--- src/io/message/mc_message.c
+++ src/io/message/mc_message.c
@@ -4,24 +4,68 @@
  */
 
 #include <stdlib.h>
+#include "dsa/sarray.h"
+#include "io/communication/communication.h"
 #include "io/message/mc_message.h"
+ 
+typedef struct __attribute__((packed))
+{
+  uint32_t  rest_size;
+  uint16_t  size;
+  mc_msg_id msg_id;
+  char      data[0];
+}mc_msg_pkt;
 
 typedef struct __attribute__((packed))
 {
-  uint32_t rest_size;
-  mc_msg_id msg_id;
-  char data[0];
-}mc_msg_pkt;
+  mc_msg_receive_cb on_receive;
+  mc_msg_id         id;
+}id_node;
 
-
-
-mc_result_u32 mc_msg_get_alloc_size(uint32_t recv_pool_size)
+struct _mc_msg
 {
-  return mc_result_u32(0, MC_SUCCESS);
+  mc_comm*  comm;
+  mc_sarray ids;
+  mc_buffer recv_pool;// TODO(MN): Light buffer with call fn on specific size?
+  uint32_t  recv_pool_stored;
+};
+
+
+mc_result_u32 mc_msg_get_alloc_size(mc_msg_cfg config)
+{
+  if ((NULL == config.io.recv) || (NULL == config.io.send) ||
+    (0 == config.recv_pool_size) || (0 == config.window_size) ||
+    (0 == config.ids_capacity)) {
+    return mc_result_u32(0, MC_ERR_INVALID_ARGUMENT);
+  }
+
+  mc_result_u32 result = mc_comm_get_alloc_size(config.window_size, 3);
+  if (MC_SUCCESS != result.error) {
+    return result;
+  }
+  const uint32_t comm_size = result.value;
+
+  result = mc_sarray_required_size(sizeof(id_node), config.ids_capacity);
+  if (MC_SUCCESS != result.error) {
+    return result;
+  }
+  const uint32_t ids_size = result.value;
+
+  const uint32_t size = sizeof(mc_msg) + comm_size + config.recv_pool_size;
+  return mc_result_u32(size, MC_SUCCESS);
 }
 
-mc_result_ptr mc_msg_init(uint32_t recv_pool_size)
+mc_result_ptr mc_msg_init(mc_buffer alloc_buffer, mc_msg_cfg config)
 {
+  const mc_result_u32 result = mc_msg_get_alloc_size(config);
+  if (MC_SUCCESS != result.error) {
+    return mc_result_ptr(NULL, result.error);
+  }
+  if (mc_buffer_get_size(alloc_buffer) < result.value) {
+    return mc_result_ptr(NULL, MC_ERR_BAD_ALLOC);
+  }
+
+  mc_msg* this = (mc_msg*)alloc_buffer.data;
   return mc_result_ptr(NULL, MC_SUCCESS);
 }
 
