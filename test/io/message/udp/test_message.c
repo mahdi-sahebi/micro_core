bd270166237795e96d5ba3da2e02bb9801e05238
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-08-22 05:33:14 +0330

    [TEST]: Signal test is added

--- test/io/message/udp/test_message.c
+++ test/io/message/udp/test_message.c
@@ -144,7 +144,7 @@ static int singly_low_lossy()
 static int singly_high_lossy()
 {
   cfg_set_loss_rate(95);
-  cfg_set_iterations(100);
+  cfg_set_iterations(50);
   const int result = singly_direction();
   cfg_set_loss_rate(0);
   return result;
