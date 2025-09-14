b581200b4a36c88da0bb95f2f1fc8b56ab5760cb
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-09-14 12:26:29 +0330

    [UPDATE]: IO send is defined

--- src/io/communication/mc_io.h
+++ src/io/communication/mc_io.h
@@ -5,12 +5,12 @@
 #include "alg/mc_buffer.h"
 #include "mc_base.h"// TODO(MN): Delete
 
+typedef void (*io_data_ready_cb)(const mc_buffer buffer, void* arg);
+
 
 void io_init(mc_io* this, mc_io io);
-// void io_recv(mc_io* this, mc_buffer buffer, mc_data_ready_cb data_ready, void* arg);
-typedef void (*io_data_ready_cb)(const mc_buffer buffer, void* arg);
 void io_recv(mc_comm* this, io_data_ready_cb data_ready, void* arg);
-bool send_buffer(mc_comm* this, const void* buffer, uint32_t size);
+bool io_send(mc_comm* this, const void* buffer, uint32_t size);
 
 
 #endif /* MC_IO_COMMUNICATION_IO_H_ */
