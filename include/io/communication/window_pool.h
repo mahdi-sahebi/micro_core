3dade77f69d62d3e501ac15f6a986bed46c62191
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-09-29 13:46:06 +0100

    [UPDATE]: Clean up

---
+++ include/io/communication/window_pool.h
@@ -0,0 +1,49 @@
+/* TODO(MN): This container is not a queue, so doesn't need to have enqueue and dequeue.
+ * Use const for all arguments
+ */
+
+#ifndef MC_MESSAGE_WINDOW_POOL_H_
+#define MC_MESSAGE_WINDOW_POOL_H_
+
+#include "core/time.h"
+#include "alg/algorithm.h"
+#include "window.h"
+
+
+typedef uint8_t mc_wnd_idx;
+
+typedef struct __attribute__((packed))
+{
+  mc_time_t  update_time;// TODO(MN): Rename to _ms
+  mc_pkt_id  bgn_id;// TODO(MN): Handle overflow
+  mc_pkt_id  end_id;// TODO(MN): Remove
+  uint16_t   window_size;
+  uint16_t   stored_size;// TODO(MN): Use for sender. Remove it. use packet.size. rename to last_window_stored
+  mc_wnd_idx bgn_index;
+  mc_wnd_idx capacity;
+  wnd_t      windows[0];
+}wndpool_t;
+
+typedef void (*wndpool_cb_done)(mc_buffer buffer, void* arg);
+
+#define WNDPOOL_GET_WINDOWS_SIZE(WINDOW_SIZE, CAPACITY)\
+  ((CAPACITY) * wnd_get_size(WINDOW_SIZE))
+
+
+void     wndpool_init          (wndpool_t* this, uint16_t window_size, uint8_t capacity);
+void     wndpool_clear         (wndpool_t* this);
+bool     wndpool_contains      (wndpool_t* this, mc_pkt_id id);
+wnd_t*   wndpool_get           (wndpool_t* this, mc_pkt_id id);
+wnd_t*   wndpool_get_last      (wndpool_t* this);
+uint8_t  wndpool_get_count     (wndpool_t* this);
+uint8_t  wndpool_get_capacity  (wndpool_t* this);
+bool     wndpool_update        (wndpool_t* this, mc_buffer buffer, mc_pkt_id id);
+bool     wndpool_ack           (wndpool_t* this, mc_pkt_id id);
+uint32_t wndpool_read          (wndpool_t* this, mc_buffer buffer);
+uint32_t wndpool_write         (wndpool_t* this, mc_buffer buffer, wndpool_cb_done on_done, void* arg);
+bool     wndpool_is_empty      (wndpool_t* this);
+void     wndpool_update_header (wndpool_t* this);
+bool     wndpool_has_incomplete(wndpool_t* this);
+
+
+#endif /* MC_MESSAGE_WINDOW_POOL_H_ */
