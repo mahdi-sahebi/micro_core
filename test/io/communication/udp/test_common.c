44d1faa5d78eb787e726eb23e2399b41c6be0cdb
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-10-04 23:10:52 +0100

    [TEST]: Code coverage of frame is become 100%

--- test/io/communication/udp/test_common.c
+++ test/io/communication/udp/test_common.c
@@ -139,14 +139,26 @@ uint32_t socket_read(int socket_fd, void* data, uint32_t size)
     bit_corruption = !bit_corruption;
     RecvFailedCounter++;
     
+    /* Header Corruption */
+    static uint8_t hdr_corruption = 0;
+    hdr_corruption++;
+    if (hdr_corruption > 30) {
+      hdr_corruption = 0;
+      ((uint8_t*)data)[1] ^= 1;
+      
+      /* Incomplete Frame */
+      read_size *= 0.74F;
+    }
+
     if (!bit_corruption) {
       return 0;
     }
   }
   
   if (0 != read_size) {
+    /* Data corruption */
     if (packetDrop && bit_corruption) {
-      ((uint8_t*)data)[29] ^= 1;
+        ((uint8_t*)data)[29] ^= 1;
     }
   }
   
