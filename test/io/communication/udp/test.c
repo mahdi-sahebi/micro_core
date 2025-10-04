02ba44ab9b7b87beb341f5c9480eb80abcb02372
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2025-10-04 15:46:37 +0100

    [TEST]: Invalid argument test cases are added, 100% communication code coverage

--- test/io/communication/udp/test.c
+++ test/io/communication/udp/test.c
@@ -73,6 +73,56 @@ static int invalid_creation()
   return MC_SUCCESS;
 }
 
+static int invalid_argument()
+{
+  char temp[10];
+  mc_ptr result_ptr = {0};
+  mc_u32 result_u32;
+  mc_bool result_bool;
+  mc_err error;
+
+  char alloc_buffer[200];
+  const mc_comm_cfg config = mc_comm_cfg(mc_io(io_recv, io_send), mc_comm_wnd(15, 1), mc_comm_wnd(15, 1));
+  result_u32 = mc_comm_req_size(config);
+  const mc_ptr result = mc_comm_init(mc_buffer(alloc_buffer, result_u32.value), config);
+  if (MC_SUCCESS != result.error) {
+    return result.error;
+  }
+  mc_comm* const com = result.data;
+
+  result_u32 = mc_comm_recv(NULL, temp, sizeof(temp), 100);
+  if ((MC_ERR_INVALID_ARGUMENT != result_u32.error) || (0 != result_u32.value)) {
+    return MC_ERR_RUNTIME;
+  }
+
+  result_u32 = mc_comm_recv(com, NULL, sizeof(temp), 100);
+  if ((MC_ERR_INVALID_ARGUMENT != result_u32.error) || (0 != result_u32.value)) {
+    return MC_ERR_RUNTIME;
+  }
+
+  result_u32 = mc_comm_send(NULL, temp, sizeof(temp), 100);
+  if ((MC_ERR_INVALID_ARGUMENT != result_u32.error) || (0 != result_u32.value)) {
+    return MC_ERR_RUNTIME;
+  }
+
+  result_u32 = mc_comm_send(com, NULL, sizeof(temp), 100);
+  if ((MC_ERR_INVALID_ARGUMENT != result_u32.error) || (0 != result_u32.value)) {
+    return MC_ERR_RUNTIME;
+  }
+  
+  error = mc_comm_update(NULL);
+  if (MC_ERR_INVALID_ARGUMENT != error) {
+    return MC_ERR_RUNTIME;
+  }
+
+  result_bool = mc_comm_flush(NULL, 100);
+  if ((MC_ERR_INVALID_ARGUMENT != result_bool.error) || (false != result_bool.value)) {
+    return MC_ERR_RUNTIME;
+  }
+
+  return MC_SUCCESS;
+}
+
 static int valid_creation()
 {
   char memory[1024];
@@ -196,6 +246,18 @@ int main()
   {
     const mc_time_t bgn_time_us = mc_now_u();
     result = invalid_creation();
+    if (MC_SUCCESS != result) {
+      printf("FAILED: %u\n\n", result);
+    } else {
+      printf("PASSED - %u(us)\n\n", (uint32_t)(mc_now_u() - bgn_time_us));
+    }
+  }
+
+
+  printf("[invalid_argument]\n");
+  {
+    const mc_time_t bgn_time_us = mc_now_u();
+    result = invalid_argument();
     if (MC_SUCCESS != result) {
       printf("FAILED: %u\n\n", result);
     } else {
