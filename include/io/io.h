f45d391452aaf01658192b47a395f18a9854a936
Mahdi Sahebi <m.nejadsahebi@live.co.uk>
2026-09-22 06:43:19 +0200

    [UPDATE] Apply MISRA C to the io/communication module
    Remove the unused receive callback type and scope documented deviations to validated generic buffers and resource-free guard-clause control flow.

--- include/io/io.h
+++ include/io/io.h
@@ -8,7 +8,6 @@
 
 typedef uint32_t (*mc_io_fn_recv)(void* const data, uint32_t size);
 typedef uint32_t (*mc_io_fn_send)(cvoid* const data, uint32_t size);
-typedef void (*mc_io_cb_receive)(cvoid* const data, uint32_t size);
 // TODO(MN): Update arg in the IO base. merge
 typedef void (*mc_cb_data_ready)(const mc_buffer buffer, void* arg);
 
